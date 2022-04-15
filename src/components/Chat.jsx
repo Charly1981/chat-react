128f8a1b579d6d9c88417c060fe58076e80639c7
Charly <charly81.zirpoli@gmail.com>
2022-04-15 15:30:23 -0300

    scroll chat

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,19 +1,35 @@
 import React from "react";
 import Agregar from "./Agregar";
+import { ChatContext } from "../context/ChatProvider";
 
 const Chat = () => {
+  const { mensajes, usuario } = React.useContext(ChatContext);
+  const refZonaChat = React.useRef(null);
+
+  React.useEffect(() => {
+    refZonaChat.current.scrollTop = refZonaChat.current.scrollHeight;
+  }, [mensajes]);
+
   return (
-    <div className="mt-3 px-2">
-      <div className="d-flex justify-content-end mb-2">
-        <span className="badge badge-pill badge-primary">
-          Mensaje del usuario
-        </span>
-      </div>
-      <div className="d-flex justify-content-start mb-2">
-        <span className="badge badge-pill badge-secondary">
-          Mensaje del usuario externo
-        </span>
-      </div>
+    <div
+      className="mt-3 px-2"
+      style={{ height: "75vh", overflowY: "scroll" }}
+      ref={refZonaChat}
+    >
+      {mensajes.map((item, index) =>
+        usuario.uid === item.uid ? (
+          <div className="d-flex justify-content-end mb-2" key={index}>
+            <span className="badge badge-pill badge-primary">{item.texto}</span>
+          </div>
+        ) : (
+          <div className="d-flex justify-content-start mb-2" key={index}>
+            <span className="badge badge-pill badge-secondary">
+              {item.texto}
+            </span>
+          </div>
+        )
+      )}
+
       <Agregar />
     </div>
   );
